9513120329140a1ab0b694fcb8443318d761d29e
VinishaGupta <vinishasgupta@gmail.com>
2026-04-15 02:54:06 +0530

    datatypes

---
+++ datatypes/a.dart
@@ -0,0 +1,16 @@
+void main(){
+    int a=2;
+    var b=5;
+    double d=5.5;
+    num n=10;
+    n=10.5;
+    print('$a  ${b}  ${d}  ${n}');
+    print("f"+"sd");
+    print('2');
+
+    int hex=0xDEADBEEF;
+    print(hex);
+
+    double exp=1.42e5;
+    print(exp);
+}
