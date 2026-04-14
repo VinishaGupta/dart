2aa1b81571b161c766034ce8cfd7a0d0f09732e2
VinishaGupta <vinishasgupta@gmail.com>
2026-04-15 03:34:51 +0530

    strings

---
+++ datatypes/strings.dart
@@ -0,0 +1,19 @@
+void main(){
+  String s="abc";
+  print("${s.toUpperCase()} xyz");
+
+  String msg = '''
+Hello
+Welcome
+To Dart
+''';
+
+print(msg);
+
+var s1 = 'Hello '
+        'World';
+print(s1);
+
+String heart = '\u2665';
+print(heart);
+}
