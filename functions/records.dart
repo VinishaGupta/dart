c3bbd541b4baba553221ab992dd77ffd7fb786d0
VinishaGupta <vinishasgupta@gmail.com>
2026-04-18 16:26:47 +0530

    destructuring

--- functions/records.dart
+++ functions/records.dart
@@ -1,8 +1,17 @@
 void main(){
 var abc=fun(2, "xyz");
 print(abc);
+
+var (a,b)=fun2();
+print(a);
+print(b);
 }
 
 (int a,String b) fun(int a,String b){
   return (a,b);
+}
+
+// destructuring
+(int a,double d) fun2(){
+  return(1,5.5);
 }
