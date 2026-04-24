94866303cdf738c039aae04fa91141f8265198de
VinishaGupta <vinishasgupta@gmail.com>
2026-04-24 18:08:05 +0530

    constructor

---
+++ 04-classes/constructors.dart
@@ -0,0 +1,16 @@
+void main(){
+  
+}
+
+class Student{
+  int? age;
+  String? name;
+
+  Student(this.age,this.name);
+
+  Student.defaultStudent(){
+    this.age=0;
+    this.name="student";
+  }
+
+}
