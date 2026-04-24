e72ea4d821a13bdb1da46788aa3c567e47572cab
VinishaGupta <vinishasgupta@gmail.com>
2026-04-25 00:36:58 +0530

    encapsulation

--- 04-classes/encapsulation.dart
+++ 04-classes/encapsulation.dart
@@ -4,10 +4,29 @@ class Student1{
   String name="lily";
 }
 
+
+// without
+
+class Student2{
+  String _name="";
+
+  void setName(String name){
+    this._name=name;
+  }
+
+  String getName(){
+    return _name;
+  }
+}
+
 void main(){
   var s=Student1();
   s.name="abc";
 
   var s1=Student1();
   print(s1.name);
+
+  var s2=Student2();
+  s2.setName("abc");
+  print(s2.getName());
 }
