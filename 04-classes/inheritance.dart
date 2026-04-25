c929f7052d99f105e3a03ee19506a6d91e18ff90
VinishaGupta <vinishasgupta@gmail.com>
2026-04-25 16:42:39 +0530

    inheritance

---
+++ 04-classes/inheritance.dart
@@ -0,0 +1,29 @@
+class Animal{
+  int age=10;
+
+  void eat(){
+    print("eating");
+  }
+}
+
+class Dog extends Animal{
+  int age=15;
+
+  void bark(){
+    print("barking");
+  }
+
+  
+}
+
+void main(){
+  var a=Animal();
+  print(a.age);
+  a.eat();
+
+  var b=Dog();
+  print(b.age);
+  b.bark();
+  b.eat();
+  
+}
