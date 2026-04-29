21e209595959ee2d43a282e119c6487adfc86da2
Vinisha Gupta <vinishasgupta@gmail.com>
2026-04-29 17:43:33 +0530

    futures

---
+++ 04-classes/enums.dart
@@ -0,0 +1,13 @@
+enum Color {
+  red,
+  green,
+  blue
+}
+
+void main() {
+  Color c = Color.red;
+
+  print(c); // Color.red
+
+  print(Color.values);
+}
---
+++ 04-classes/futures.dart
@@ -0,0 +1,10 @@
+void main(){
+  void main() async {
+  print("Start");
+
+  String data = await getData();
+
+  print(data);
+  print("End");
+}
+}
